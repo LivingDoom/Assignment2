4eaca77df6907110f13a5c40872a3f03069a3e13
S407912 <Ibrahim.najjarine@gmail.com>
2026-08-18 20:05:23 +0800

    Initial commit

---
+++ cipher.py
@@ -0,0 +1,25 @@
+"""
+Assignment 2
+
+Cipher.py
+
+Group Name: [DAN/EXT12] 
+Group Members: 
+[Kayla Edwards]                 -           [S396018] 
+[Jonathan William McPhail]      -           [S368879] 
+[Ibrahim Najjarine]             -           [S407912] 
+[Glen Mark Pasigna]             -           [S328808] 
+
+
+"""
+
+# Task 1
+
+"""
+Question 1
+Write a program in a file named cipher.py that reads the text file raw_text.txt ,
+encrypts its contents using the scheme described below, and writes the result to
+encrypted_text.txt . You must then write a function that decrypts that file, and a
+function that verifies the decryption was successful.
+
+"""
